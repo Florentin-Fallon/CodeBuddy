7da9fd61cf9a4acddd573106621ac81d3de49949
FlorentinFallon <119119355+FlorentinFallon@users.noreply.github.com>
2024-12-06 17:17:47 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-center text-red-600 text-3xl'>Salut ma chérie d'amour d'amour !!</h1>
+    </div>
+  );
+}
+
+export default App;
