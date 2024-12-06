c434cb8175fa341f788399acb6757fdd899a1a0c
FlorentinFallon <119119355+FlorentinFallon@users.noreply.github.com>
2024-12-06 17:37:56 +0100

    create more details

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1 className='text-center text-red-600 text-3xl'>Salut ma chérie d'amour d'amour !!</h1>
+      <h1 className='text-center font-bold my-5 text-green-500 text-3xl'>Salut ma chérie d'amour d'amour !!</h1>
     </div>
   );
 }
