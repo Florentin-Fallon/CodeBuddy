197684fa393e9e07b1164b6312fd03a2238fb964
FlorentinFallon <119119355+FlorentinFallon@users.noreply.github.com>
2024-12-06 17:42:47 +0100

    modify title

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1 className='text-center font-bold my-5 text-green-500 text-3xl'>Salut ma chérie d'amour d'amour !!</h1>
+      <h1 className='text-center font-bold my-5 text-green-500 text-3xl'>Bienvenue sur CodeBuddy</h1>
     </div>
   );
 }
